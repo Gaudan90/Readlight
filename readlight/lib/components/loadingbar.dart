938fd25f0b6b710dea471188fa8c9262f9dbe9c5
Gaudan90 <daniele.gauzolino@edu.itspiemonte.it>
2024-12-23 15:30:15 +0100

    wip: first commit github desktop

---
+++ readlight/lib/components/loadingbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class LoadingBar extends StatelessWidget {
+  const LoadingBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
