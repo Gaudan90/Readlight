7ee790157c75473b2e11b523aa15635837263123
Gaudan90 <daniele.gauzolino@edu.itspiemonte.it>
2024-12-28 19:34:21 +0100

    fix: column scrollable, increased padding

--- readlight/lib/screens/login_screen.dart
+++ readlight/lib/screens/login_screen.dart
@@ -13,77 +13,79 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.onSurfaceVariant,
        body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Animazione per immagine SVG 1
-              ZoomIn(
-                duration: const Duration(seconds: 4),
-                child: SvgPicture.asset(
-                  'lib/assets/readlight_book.svg',
-                  height: 100,
-                  width: 100,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Animazione per immagine SVG 1
+                ZoomIn(
+                  duration: const Duration(seconds: 4),
+                  child: SvgPicture.asset(
+                    'lib/assets/readlight_book.svg',
+                    height: 100,
+                    width: 100,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              // Animazione per immagine SVG 2
-              ZoomIn(
-                duration: const Duration(seconds: 8),
-                child: SvgPicture.asset(
-                  'lib/assets/readlight.svg',
-                  height: 100,
-                  width: 100,
+                const SizedBox(height: 45),
+                // Animazione per immagine SVG 2
+                ZoomIn(
+                  duration: const Duration(seconds: 8),
+                  child: SvgPicture.asset(
+                    'lib/assets/readlight.svg',
+                    height: 100,
+                    width: 100,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              // Animazione per Button 1
-              ZoomIn(
-                duration: const Duration(seconds: 12),
-                child: CustomButton(
-                  text: 'LOGIN',
-                  isLoginButton: true,
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => const NextScreenLogin()),
-                    );
-                  },
+                const SizedBox(height: 45),
+                // Animazione per Button 1
+                ZoomIn(
+                  duration: const Duration(seconds: 12),
+                  child: CustomButton(
+                    text: 'LOGIN',
+                    isLoginButton: true,
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => const NextScreenLogin()),
+                      );
+                    },
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              // Animazione per Button 2
-              ZoomIn(
-                duration: const Duration(seconds: 16),
-                child: CustomButton(
-                  text: 'CONTINUE Without Account',
-                  onPressed: () {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => const NextScreenLogin()),
-                    );
-                  },
+                const SizedBox(height: 45),
+                // Animazione per Button 2
+                ZoomIn(
+                  duration: const Duration(seconds: 16),
+                  child: CustomButton(
+                    text: 'CONTINUE Without Account',
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => const NextScreenLogin()),
+                      );
+                    },
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              // Animazione per Button 3
-              ZoomIn(
-                duration: const Duration(seconds: 20),
-                child: CustomButton(
-                  text: 'CREATE NOW Your Account',
-                  onPressed: () {
-                  },
+                const SizedBox(height: 45),
+                // Animazione per Button 3
+                ZoomIn(
+                  duration: const Duration(seconds: 20),
+                  child: CustomButton(
+                    text: 'CREATE NOW Your Account',
+                    onPressed: () {
+                    },
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              ZoomIn(
-                duration: const Duration(seconds: 24),
-                child: CustomButton(
-                  text: 'WATCH VIDEO',
-                  onPressed: () {
-                  },
+                const SizedBox(height: 45),
+                ZoomIn(
+                  duration: const Duration(seconds: 24),
+                  child: CustomButton(
+                    text: 'WATCH VIDEO',
+                    onPressed: () {
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
